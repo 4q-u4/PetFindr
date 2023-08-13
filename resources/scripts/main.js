cd6189cae7cfb661f0bc1c8ee837c0e85e8a8ed1
daniellmadi <se.danielmadi@gmail.com>
2023-08-13 22:21:38 +0300

    Phase 1 Done

--- resources/scripts/main.js
+++ resources/scripts/main.js
@@ -1,13 +1,35 @@
 //! Frontend Javascript - PetFindr Senior Project
-  document.addEventListener("DOMContentLoaded", function () {
-    const scrollButton = document.querySelector(".scroll-button");
-    const targetContainer = document.querySelector(".section-description");
-
+document.addEventListener("DOMContentLoaded", function () {
+  const scrollButton = document.querySelector(".scroll-button");
+  
+  if (scrollButton) {
     scrollButton.addEventListener("click", function () {
-      window.scrollTo({
-        top: targetContainer.offsetTop,
-        behavior: "smooth",
-      });
+      const buttonContainer = document.querySelector(".section-title");
+      
+      if (buttonContainer) {
+        window.scrollTo({
+          top: buttonContainer.offsetTop,
+          behavior: "smooth",
+        });
+      }
     });
-  });
+  }
+});
 
+
+document.addEventListener("DOMContentLoaded", function () {
+  const scrollButton = document.querySelector(".scroll-button-one");
+  
+  if (scrollButton) {
+    scrollButton.addEventListener("click", function () {
+      const buttonContainer = document.querySelector(".card-container");
+      
+      if (buttonContainer) {
+        window.scrollTo({
+          top: buttonContainer.offsetTop,
+          behavior: "smooth",
+        });
+      }
+    });
+  }
+});
